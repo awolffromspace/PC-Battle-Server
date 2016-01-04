0a0be8724c24972fab4ab3a9dfe13ed9943b85b8
awolffromspace <ewolft@gmail.com>
2016-01-03 23:08:43 -0500

    Add unreleased hidden abilities for XY

--- mods/xy/formats-data.js
+++ mods/xy/formats-data.js
@@ -1,6 +1,78 @@
 'use strict';
 
 exports.BattleFormatsData = {
+	chikorita: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	bayleef: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	meganium: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	cyndaquil: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	quilava: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	typhlosion: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	totodile: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	croconaw: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	feraligatr: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	snivy: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	servine: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	serperior: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	tepig: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	pignite: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	emboar: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	oshawott: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	dewott: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	samurott: {
+		inherit: true,
+		unreleasedHidden: true
+	},
 	landorus: {
 		inherit: true,
 		tier: "OU"
@@ -8,5 +80,29 @@ exports.BattleFormatsData = {
 	greninja: {
 		inherit: true,
 		tier: "OU"
+	},
+	skrelp: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	dragalge: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	tyrunt: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	tyrantrum: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	amaura: {
+		inherit: true,
+		unreleasedHidden: true
+	},
+	aurorus: {
+		inherit: true,
+		unreleasedHidden: true
 	}
 };
