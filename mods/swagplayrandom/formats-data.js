21ef3d72df004ab47a7bed3deecab7ede394976c
awolffromspace <ewolft@gmail.com>
2016-01-31 02:59:31 -0500

    Update formats-data.js

--- mods/swagplayrandom/formats-data.js
+++ mods/swagplayrandom/formats-data.js
@@ -4,44 +4,44 @@ exports.BattleFormatsData = {
 	murkrow: {
 		randomBattleMoves: ["swagger","foulplay","substitute","roost"],
 		requiredItem: "Eviolite",
-		tier: "LC Uber"
+		tier: "LC Uber",
 	},
 	sableye: {
 		randomBattleMoves: ["swagger","foulplay","substitute","recover"],
 		requiredItem: "Leftovers",
-		tier: "(OU)"
+		tier: "UU",
 	},
 	sableyemega: {
-		tier: "OU"
+		tier: "OU",
 	},
 	banette: {
 		randomBattleMoves: ["swagger","foulplay","substitute","thunderwave"],
 		requiredItem: "Banettite",
-		tier: "(RU)"
+		tier: "PU",
 	},
 	purrloin: {
 		randomBattleMoves: ["swagger","foulplay","substitute","thunderwave"],
 		requiredItem: "Leftovers",
-		tier: "LC"
+		tier: "LC",
 	},
 	liepard: {
 		randomBattleMoves: ["swagger","foulplay","substitute","thunderwave"],
 		requiredItem: "Leftovers",
-		tier: "NU"
+		tier: "NU",
 	},
 	tornadus: {
 		randomBattleMoves: ["swagger","foulplay","substitute","protect"],
 		requiredItem: "Leftovers",
-		tier: "BL2"
+		tier: "BL2",
 	},
 	thundurus: {
 		randomBattleMoves: ["swagger","foulplay","substitute","thunderwave"],
 		requiredItem: "Leftovers",
-		tier: "OU"
+		tier: "OU",
 	},
 	klefki: {
 		randomBattleMoves: ["swagger","foulplay","substitute","thunderwave"],
 		requiredItem: "Leftovers",
-		tier: "OU"
+		tier: "OU",
 	}
 };
