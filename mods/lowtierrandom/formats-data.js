f758a07fd09af9a75a264c18eaa668830b7e9579
awolffromspace <ewolft@gmail.com>
2016-01-31 02:54:22 -0500

    Update Low Tier Random

--- mods/lowtierrandom/formats-data.js
+++ mods/lowtierrandom/formats-data.js
@@ -1,22 +1,64 @@
 'use strict';
 
 exports.BattleFormatsData = {
+	charizardmegax: {
+		tier: "OU",
+	},
+	charizardmegay: {
+		tier: "OU",
+	},
+	blastoisemega: {
+		tier: "UU",
+	},
+	beedrillmega: {
+		tier: "UU",
+	},
 	kangaskhanmega: {
-		tier: "Uber"
+		tier: "Uber",
 	},
 	pinsirmega: {
-		tier: "BL"
+		tier: "OU",
+	},
+	aerodactylmega: {
+		tier: "UU",
+	},
+	ampharosmega: {
+		tier: "UU",
 	},
 	houndoommega: {
-		tier: "BL2"
+		tier: "BL2",
+	},
+	sceptilemega: {
+		tier: "UU",
 	},
 	gallademega: {
-		tier: "BL"
+		tier: "BL",
 	},
 	mawilemega: {
-		tier: "Uber"
+		tier: "Uber",
+	},
+	aggronmega: {
+		tier: "UU",
 	},
 	medichammega: {
-		tier: "BL"
-	}
+		tier: "OU",
+	},
+	manectricmega: {
+		tier: "OU",
+	},
+	sharpedomega: {
+		tier: "UU",
+	},
+	altariamega: {
+		tier: "OU",
+	},
+	absolmega: {
+		tier: "UU",
+	},
+	lopunnymega: {
+		tier: "OU",
+	},
+	dianciemega: {
+		tier: "OU",
+	},
 };
