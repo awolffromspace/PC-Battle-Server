a4081224900c804b264ca1830430542768117464
awolffromspace <ewolft@gmail.com>
2016-01-30 20:25:55 -0500

    Organize Random formats

--- chat-plugins/poll.js
+++ chat-plugins/poll.js
@@ -143,7 +143,7 @@ exports.commands = {
 	tourneypoll: 'tourpoll',
 	tourpoll: function (target, room, user) {
 		if (!this.can('poll', null, room)) return false;
-		this.parse("/poll Tournament format?," + "OU, Ubers, UU, RU, NU, PU, LC, Monotype, Random, 1v1 Random, Uber Random, High Tier Random, Low Tier Random, LC Random, Monotype Random, Generational Random, Color Random, Inverse Random, Community Random, Orb Random, Hoenn Random, Hoenn Weather Random, Super Smash Bros. Random, Winter Wonderland, Super Staff Bros., Metronome 3v3 Random, Doubles Random, Triples Random, Battle Factory, Hackmons Cup, [Seasonal] Random, [Gen 5] Random, [Gen 2] Random, [Gen 1] Random");
+		this.parse("/poll Tournament format?," + "OU, Ubers, UU, RU, NU, PU, LC, Monotype, Random, 1v1 Random, High Tier Random, Low Tier Random, Uber Random, LC Random, Monotype Random, Generational Random, Color Random, Inverse Random, Community Random, Orb Random, Hoenn Random, Hoenn Weather Random, Super Smash Bros. Random, Winter Wonderland, Super Staff Bros., Metronome 3v3 Random, Doubles Random, Triples Random, Battle Factory, Hackmons Cup, [Seasonal] Random, [Gen 5] Random, [Gen 2] Random, [Gen 1] Random");
 	},
 
 	teampoll: function (target, room, user) {
@@ -156,7 +156,7 @@ exports.commands = {
 	randpoll: 'randompoll',
 	randompoll: function (target, room, user) {
 		if (!this.can('poll', null, room)) return false;
-		this.parse("/poll Tournament format?," + "Random, 1v1 Random, Uber Random, High Tier Random, Low Tier Random, LC Random, Monotype Random, Generational Random, Color Random, Inverse Random, Community Random, Orb Random, Hoenn Random, Hoenn Weather Random, Super Smash Bros. Random, Winter Wonderland, Super Staff Bros., Metronome 3v3 Random, Doubles Random, Triples Random, Battle Factory, Hackmons Cup, [Seasonal] Random, [Gen 5] Random, [Gen 2] Random, [Gen 1] Random");
+		this.parse("/poll Tournament format?," + "Random, 1v1 Random, High Tier Random, Low Tier Random, Uber Random, LC Random, Monotype Random, Generational Random, Color Random, Inverse Random, Community Random, Orb Random, Hoenn Random, Hoenn Weather Random, Super Smash Bros. Random, Winter Wonderland, Super Staff Bros., Metronome 3v3 Random, Doubles Random, Triples Random, Battle Factory, Hackmons Cup, [Seasonal] Random, [Gen 5] Random, [Gen 2] Random, [Gen 1] Random");
 	},
 
 	vote: function (target, room, user) {
