3e1843488ded278ae3b0ca0d666df80712d2edfb
awolffromspace <ewolft@gmail.com>
2016-04-03 14:13:48 -0400

    Update LotW

--- rooms.js
+++ rooms.js
@@ -941,7 +941,7 @@ let BattleRoom = (() => {
 			// Battle Point Winnings
 			//
 
-			if (this.format === 'lotw17random') {
+			if (this.format === 'lotw18uberrandom') {
 				let wid = toId(winner);
 				Database.read('bp', wid, function (err, initial) {
 					if (err) throw err;
