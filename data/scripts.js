651151efa140116baa7d08a7c2a447d501fbd489
awolffromspace <ewolft@gmail.com>
2016-06-02 21:36:29 -0400

    Update Uber Random

--- data/scripts.js
+++ data/scripts.js
@@ -4005,9 +4005,6 @@ exports.BattleScripts = {
 			case 'Gourgeist':
 				if (this.random(4) >= 1) continue;
 				break;
-			case 'Hoopa':
-				if (this.random(2) >= 1) continue;
-				break;
 			case 'Meloetta':
 				if (this.random(2) >= 1) continue;
 				break;
