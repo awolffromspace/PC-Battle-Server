e63c583e33e860e2b6aa2e460d76764afd30faa7
awolffromspace <ewolft@gmail.com>
2016-06-02 20:59:12 -0400

    Allow global mods to set PotD

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -1486,7 +1486,7 @@ exports.commands = {
 	 *********************************************************/
 
 	potd: function (target, room, user) {
-		if (!this.can('potd')) return false;
+		if (!this.can('ban')) return false;
 
 		Config.potd = target;
 		Simulator.SimulatorProcess.eval('Config.potd = \'' + toId(target) + '\'');
