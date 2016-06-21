5edb78847de37e6078ac57fd4bba85f248af7790
awolffromspace <ewolft@gmail.com>
2016-06-21 16:02:53 -0400

    Update define.js

--- chat-plugins/define.js
+++ chat-plugins/define.js
@@ -20,7 +20,7 @@ exports.commands = {
 		if (!target) return this.parse('/help define');
 		target = toId(target);
 		if (target > 50) return this.errorReply("Word can not be longer than 50 characters.");
-		if (!this.canBroadcast()) return;
+		if (!this.runBroadcast()) return;
 		if (!this.canTalk()) return this.errorReply("You cannot do this while unable to speak.");
 
 		var options = {
