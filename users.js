1133c90316dd0c4396b4e838d6cd2330dfc558e1
awolffromspace <ewolft@gmail.com>
2016-09-05 14:33:32 -0400

    Fix tells

--- users.js
+++ users.js
@@ -633,6 +633,7 @@ class User {
 			this.send('|nametaken|' + name + "|Your authentication token was invalid.");
 		}
 
+		if (Tells.inbox[userid]) Tells.sendTell(userid, this);
 		return false;
 	}
 	validateRename(name, tokenData, newlyRegistered, challenge) {
