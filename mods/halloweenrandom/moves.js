396fdace8269534b171bfa3fa09b4c6c5d7fbbad
awolffromspace <ewolft@gmail.com>
2016-10-19 00:04:46 -0400

    Update Halloween Random

--- mods/halloweenrandom/moves.js
+++ mods/halloweenrandom/moves.js
@@ -13,24 +13,12 @@ exports.BattleMovedex = {
 		flags: {authentic: 1},
 		volatileStatus: 'curse',
 		onTryHit: function (target, source, move) {
-			var rand = this.random(9);
-			if (rand < 3 && target.hp < target.maxhp) {
+			var rand = this.random(2);
+			if (rand < 1 && target.hp < target.maxhp) {
 				delete move.volatileStatus;
 				delete move.onHit;
 				move.heal = [1, 4];
-			} else if (rand === 3) {
-				target.trySetStatus('brn', source);
-			} else if (rand === 4) {
-				target.trySetStatus('par', source);
-			} else if (rand === 5) {
-				target.trySetStatus('slp', source);
-			} else if (rand === 6) {
-				target.trySetStatus('frz', source);
-			} else if (rand === 7) {
-				target.trySetStatus('tox', source);
-			} else if (rand === 8) {
-				target.trySetStatus('confusion', source);
-			} else if (rand === 9 && move.volatileStatus && target.volatiles.curse) {
+			} else if (move.volatileStatus && target.volatiles.curse) {
 				return false;
 			}
 		},
@@ -41,7 +29,7 @@ exports.BattleMovedex = {
 			onResidualOrder: 10,
 			onResidual: function (pokemon) {
 				this.damage(pokemon.maxhp / 4);
-			},
+			}
 		},
 		secondary: false,
 		target: "normal",
