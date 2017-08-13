333a12b147d5a9c0762b86a448904dd62b968cc4
awolffromspace <ewolft@gmail.com>
2017-08-13 03:38:14 -0400

    Fix tiered Random formats

--- data/random-teams.js
+++ data/random-teams.js
@@ -516,7 +516,9 @@ class RandomTeams extends Dex.ModdedDex {
 		}
 		let battleForme = this.checkBattleForme(template);
 		if (battleForme && battleForme.randomBattleMoves && (battleForme.isMega ? !teamDetails.megaStone : this.random(2))) {
-			template = this.getTemplate(template.otherFormes.length >= 2 ? template.otherFormes[this.random(template.otherFormes.length)] : template.otherFormes[0]);
+			if (this.random(4) > 0) {
+				template = this.getTemplate(template.otherFormes.length >= 2 ? template.otherFormes[this.random(template.otherFormes.length)] : template.otherFormes[0]);
+			}
 		}
 
 		let movePool = (template.randomBattleMoves ? template.randomBattleMoves.slice() : Object.keys(template.learnset));
